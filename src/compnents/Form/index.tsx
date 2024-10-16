4411f7b07f94f6722fd49adc0b6e11766d040f00
akulSingh18 <141423846+akulSingh18@users.noreply.github.com>
2024-10-16 18:32:02 +0000

    todolist half done

--- src/compnents/Form/index.tsx
+++ src/compnents/Form/index.tsx
@@ -3,29 +3,61 @@ import { useEffect, useState } from 'react';
 import TodoDetails from '../TodoDetails/index';
 
 
+ export type todoObjType = {
+  val : string,
+  state : string,
+  color : string,
+}
+
+
+
 const Form = () =>{
 
+
+  // const obj = {
+  //   value :"akul",
+  //   state : "pending",
+  //   color: " white"
+  // }
+
+  
+
   const [formInput,setFormInput] = useState<string>("");
-  const [formArr,setFormArr] = useState<string[]>([]);
+  const [formArr,setFormArr] = useState<todoObjType[]>([]);
 
  const  handleChange = (e:any) => {
    setFormInput(e.target.value);
   }
 
-  const handleClick = (e:any) => { 
-    setFormArr([...formArr,formInput]);
+  const handleAdd = (e:any) => { 
+
+    let v = formInput;
+
+    if(formInput.length > 14){
+       v = formInput.substring(0, 14);
+    }
+    const obj = {
+       val : v,
+       state : "pending",
+       color: "black"
+     }
+    
+    console.log(obj);
+    setFormArr([...formArr,obj]);
     setFormInput("");
   }
 
   const handleDelete = (index:number) =>{
-   
-    formArr.splice(index,1);
+    formArr[index].state = "delete";
+    setFormArr([...formArr]);
+  }
+
+  const handleDone = (index:number) =>{
+    formArr[index].state = "done";
     setFormArr([...formArr]);
     
   }
-  // useEffect(()=>{
-  //   // console.log(formArr , "array_value");
-  // },[formArr])
+
 
   
     
@@ -33,11 +65,12 @@ const Form = () =>{
     <div className="form">
       <div className="form-input-and-button">
           <input onChange={handleChange}  className="form-input" value={formInput}  type="text"  placeholder="Add New Todo"></input> 
-          <button onClick={(e)=>{handleClick(e)}} disabled={formInput === ''} className="add-button">Add</button>
+          <button onClick={(e)=>{handleAdd(e)}} disabled={formInput === ''} className="add-button">Add</button>
       </div>
       <TodoDetails 
         formArr={formArr}
         handleDelete={handleDelete}
+        handleDone={handleDone}
         />
       
      
--- src/compnents/TodoDetails/index.tsx
+++ src/compnents/TodoDetails/index.tsx
@@ -1,13 +1,15 @@
 import "./style.css"
 import dustbin from "../../../dustbin.png";
-import emptyCheckbox from "../../../icons8-check-box-96.png";
+import emptyCheckbox from "../../../empty-check-box.png";
+import checkBox from "../../../check-box.png";
+import {todoObjType} from "../Form";
 
 
 
 
-const TodoDetails = (props :{formArr: string[], handleDelete :(arg: number) => void }) =>{
+const TodoDetails = (props :{formArr: todoObjType[], handleDelete :(arg: number) => void,handleDone :(arg:number) =>void }) =>{
 
-  const { formArr, handleDelete } = props;
+  const { formArr, handleDelete,handleDone } = props;
   
   // const handleDone = (e,index:number) => {
 
@@ -15,23 +17,52 @@ const TodoDetails = (props :{formArr: string[], handleDelete :(arg: number) => v
     
   // }
 
+  const getStateClass = (state:string) =>{
+
+    if(state === "done"){
+      return "green";
+    }else if(state === "delete"){
+      return "red";
+    }
+    return "white";
+  }
+
+
+  const checkboxfunc = (state:string) =>{
+
+    if(state === "done"){
+      return checkBox;
+    }
+
+    return emptyCheckbox;
+  }
+
 
   return (
     <div className="todoDetails"> 
-      <div className="todo-itemss-wrapper">
+      <div className="todo-itemss-wrapper">                                
         {
           formArr?  (formArr.length? 
 
-          formArr.map((value:String,index:number)=> {
+          formArr.map((value:todoObjType,index:number)=> {
            return (
                    <div className="todo-item-wrap">
                     
-                     <div className="todoDetails-items">
-                       {value}</div>
-                     <button onClick={(e)=>{handleDelete(index)}}className="todoDetails-items-button">
-                       <img className="dustbin" src={dustbin}/>
-                     </button>
-                   </div>
+                      <div  
+                         className={`todoDetails-items ${ getStateClass(value.state) }`}>
+                          <div className="check-box-cont ">
+                             <img onClick={(e)=>{handleDone(index)}}  className={`empty-checkBox-img ${ getStateClass(value.state) }`} 
+                               src={`${checkboxfunc(value.state)}`}/>
+                          </div>
+                          {value?.val}
+                           {/* <div>
+                             <img src={dd}/>
+                          </div> */}
+                      </div>
+                      <button onClick={(e)=>{handleDelete(index)}}className="todoDetails-items-button">
+                         <img className="dustbin-img" src={dustbin}/>
+                      </button>
+                    </div>
                   )
           }) : null ): null
         }
